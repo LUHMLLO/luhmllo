0a5d569729c2822f6d63128918911940fd57c626
Luis Melo <luis.melo@hytechgizmo.com>
2023-10-26 17:00:07 -0400

    working on button colors

--- src/components/molecules/button.ts
+++ src/components/molecules/button.ts
@@ -64,8 +64,8 @@ export class Button extends LitElement {
 
 			:host(:focus),
 			:host([emphasis='low']:focus) {
-				background-color: var(--clr-tertiary--focus);
-				outline-color: var(--clr-onTertiary--focus);
+				outline-color: var(--clr-tertiary--focus);
+				color: var(--clr-onTertiary--focus);
 			}
 
 			@media (hover: hover) {
@@ -84,48 +84,48 @@ export class Button extends LitElement {
 		`,
 		css`
 			:host([emphasis='medium']) {
-				background-color: var(--clr-tertiary);
-				color: var(--clr-onTertiary);
+				background-color: var(--clr-secondary);
+				color: var(--clr-onSecondary);
 			}
 
 			:host([emphasis='medium']:focus) {
-				background-color: var(--clr-tertiary--focus);
-				outline-color: var(--clr-onTertiary--focus);
+				outline-color: var(--clr-secondary--focus);
+				color: var(--clr-onSecondary--focus);
 			}
 
 			@media (hover: hover) {
 				:host([emphasis='medium']:hover) {
-					background-color: var(--clr-tertiary--hover);
-					color: var(--clr-onTertiary--hover);
+					background-color: var(--clr-secondary--hover);
+					color: var(--clr-onSecondary--hover);
 				}
 			}
 
 			:host([emphasis='medium']:active) {
-				background-color: var(--clr-tertiary--active);
-				color: var(--clr-onTertiary--active);
+				background-color: var(--clr-secondary--active);
+				color: var(--clr-onSecondary--active);
 			}
 		`,
 		css`
 			:host([emphasis='high']) {
-				background-color: var(--clr-tertiary);
-				color: var(--clr-onTertiary);
+				background-color: var(--clr-primary);
+				color: var(--clr-onPrimary);
 			}
 
 			:host([emphasis='high']:focus) {
-				background-color: var(--clr-tertiary--focus);
-				outline-color: var(--clr-onTertiary--focus);
+				background-color: var(--clr-primary--focus);
+				outline-color: var(--clr-onPrimary--focus);
 			}
 
 			@media (hover: hover) {
 				:host([emphasis='high']:hover) {
-					background-color: var(--clr-tertiary--hover);
-					color: var(--clr-onTertiary--hover);
+					background-color: var(--clr-primary--hover);
+					color: var(--clr-onPrimary--hover);
 				}
 			}
 
 			:host([emphasis='high']:active) {
-				background-color: var(--clr-tertiary--active);
-				color: var(--clr-onTertiary--active);
+				background-color: var(--clr-primary--active);
+				color: var(--clr-onPrimary--active);
 			}
 		`,
 	];
