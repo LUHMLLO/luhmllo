2d9f31d0e36f1b3cc26d365c297317c4a0559577
Luis Melo <luis.melo@hytechgizmo.com>
2023-10-27 11:18:45 -0400

    feeling satisfied with the current button component

--- src/components/molecules/button.ts
+++ src/components/molecules/button.ts
@@ -18,28 +18,44 @@ export class Button extends LitElement {
 	static styles = [
 		css`
 			:host {
+				--idle-bg: transparent;
+				--idle-clr: hsl(0, 0%, 6%);
+				--idle-bord: transparent;
+
+				--hover-bg: transparent;
+				--hover-clr: hsl(0, 0%, 6%);
+				--hover-bord: hsl(0, 0%, 96%);
+
+				--active-bg: transparent;
+				--active-clr: hsl(0, 0%, 6%);
+				--active-bord: hsl(0, 0%, 90%);
+
+				--focus-bg: transparent;
+				--focus-clr: hsl(0, 0%, 6%);
+				--focus-bord: var(--clr-accent);
+
 				appeareance: none;
 				box-sizing: border-box;
 				cursor: pointer;
 
 				align-items: center;
 				display: inline-flex;
-				gap: var(--scale-5xs);
 
 				font-size: var(--scale-md);
+				gap: var(--scale-5xs);
 				padding: var(--scale-xs) var(--scale-sm);
 
-				outline-color: transparent;
 				outline-style: solid;
-				outline-width: 1px;
-				outline-offset: -1px;
+				outline-width: 0.0938rem;
+				outline-offset: -0.0938rem;
 
 				height: max-content;
 				width: max-content;
 				overflow: hidden;
 
 				transition: color var(--transition-settings),
-					background-color var(--transition-settings);
+					background-color var(--transition-settings),
+					outline-color var(--transition-settings);
 			}
 
 			:host * {
@@ -58,74 +74,70 @@ export class Button extends LitElement {
 		css`
 			:host,
 			:host([emphasis='low']) {
-				background-color: var(--clr-tertiary);
-				color: var(--clr-onTertiary);
+				background-color: var(--idle-bg);
+				color: var(--idle-clr);
+				outline-color: var(--idle-bord);
 			}
 
 			:host(:focus),
 			:host([emphasis='low']:focus) {
-				outline-color: var(--clr-tertiary--focus);
-				color: var(--clr-onTertiary--focus);
+				background-color: var(--focus-bg);
+				color: var(--focus-clr);
+				outline-color: var(--focus-bord);
 			}
 
 			@media (hover: hover) {
 				:host(:hover),
 				:host([emphasis='low']:hover) {
-					background-color: var(--clr-tertiary--hover);
-					color: var(--clr-onTertiary--hover);
+					background-color: var(--hover-bg);
+					color: var(--hover-clr);
+					outline-color: var(--hover-bord);
 				}
 			}
 
 			:host(:active),
 			:host([emphasis='low']:active) {
-				background-color: var(--clr-tertiary--active);
-				color: var(--clr-onTertiary--active);
+				background-color: var(--active-bg);
+				color: var(--active-clr);
+				outline-color: var(--active-bord);
 			}
 		`,
 		css`
 			:host([emphasis='medium']) {
-				background-color: var(--clr-secondary);
-				color: var(--clr-onSecondary);
-			}
+				--idle-bg: hsl(0, 0%, 93%);
+				--idle-clr: hsl(0, 0%, 6%);
+				--idle-bord: transparent;
 
-			:host([emphasis='medium']:focus) {
-				outline-color: var(--clr-secondary--focus);
-				color: var(--clr-onSecondary--focus);
-			}
+				--hover-bg: hsl(0, 0%, 96%);
+				--hover-clr: hsl(0, 0%, 6%);
+				--hover-bord: transparent;
 
-			@media (hover: hover) {
-				:host([emphasis='medium']:hover) {
-					background-color: var(--clr-secondary--hover);
-					color: var(--clr-onSecondary--hover);
-				}
-			}
+				--active-bg: hsl(0, 0%, 90%);
+				--active-clr: hsl(0, 0%, 6%);
+				--active-bord: transparent;
 
-			:host([emphasis='medium']:active) {
-				background-color: var(--clr-secondary--active);
-				color: var(--clr-onSecondary--active);
+				--focus-bg: hsl(0, 0%, 93%);
+				--focus-clr: hsl(0, 0%, 6%);
+				--focus-bord: var(--clr-accent);
 			}
 		`,
 		css`
 			:host([emphasis='high']) {
-				background-color: var(--clr-primary);
-				color: var(--clr-onPrimary);
-			}
+				--idle-bg: hsl(0, 0%, 6%);
+				--idle-clr: hsl(0, 0%, 96%);
+				--idle-bord: transparent;
 
-			:host([emphasis='high']:focus) {
-				background-color: var(--clr-primary--focus);
-				outline-color: var(--clr-onPrimary--focus);
-			}
+				--hover-bg: hsl(0, 0%, 9%);
+				--hover-clr: hsl(0, 0%, 96%);
+				--hover-bord: transparent;
 
-			@media (hover: hover) {
-				:host([emphasis='high']:hover) {
-					background-color: var(--clr-primary--hover);
-					color: var(--clr-onPrimary--hover);
-				}
-			}
+				--active-bg: hsl(0, 0%, 3%);
+				--active-clr: hsl(0, 0%, 96%);
+				--active-bord: transparent;
 
-			:host([emphasis='high']:active) {
-				background-color: var(--clr-primary--active);
-				color: var(--clr-onPrimary--active);
+				--focus-bg: hsl(0, 0%, 6%);
+				--focus-clr: hsl(0, 0%, 96%);
+				--focus-bord: var(--clr-accent);
 			}
 		`,
 	];
