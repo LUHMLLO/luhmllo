0bfd119e3f37d18a5e753bbcacbf7f45a9658593
Luis Melo <luis.melo@hytechgizmo.com>
2023-10-28 12:00:45 -0400

    reduced animation duration, working on styling expandable, updated some styles on button component

--- src/components/molecules/button.ts
+++ src/components/molecules/button.ts
@@ -71,6 +71,11 @@ export class Button extends LitElement {
 			:host([rounded]) {
 				border-radius: var(--bord-radius);
 			}
+
+			:host::part(lead),
+			:host::part(trail) {
+				transition: transform var(--transition-settings),
+					rotate var(--transition-settings);
 		`,
 		css`
 			:host,
@@ -113,30 +118,30 @@ export class Button extends LitElement {
 				--hover-clr: hsl(0, 0%, 6%);
 				--hover-bord: transparent;
 
-				--active-bg: hsl(0, 0%, 90%);
+				--active-bg: hsl(0, 0%, 87%);
 				--active-clr: hsl(0, 0%, 6%);
 				--active-bord: transparent;
 
-				--focus-bg: hsl(0, 0%, 93%);
+				--focus-bg: hsl(0, 0%, 90%);
 				--focus-clr: hsl(0, 0%, 6%);
 				--focus-bord: var(--clr-accent);
 			}
 		`,
 		css`
 			:host([emphasis='high']) {
-				--idle-bg: hsl(0, 0%, 6%);
+				--idle-bg: hsl(0, 0%, 15%);
 				--idle-clr: hsl(0, 0%, 96%);
 				--idle-bord: transparent;
 
-				--hover-bg: hsl(0, 0%, 9%);
+				--hover-bg: hsl(0, 0%, 21%);
 				--hover-clr: hsl(0, 0%, 96%);
 				--hover-bord: transparent;
 
-				--active-bg: hsl(0, 0%, 3%);
+				--active-bg: hsl(0, 0%, 9%);
 				--active-clr: hsl(0, 0%, 96%);
 				--active-bord: transparent;
 
-				--focus-bg: hsl(0, 0%, 6%);
+				--focus-bg: hsl(0, 0%, 12%);
 				--focus-clr: hsl(0, 0%, 96%);
 				--focus-bord: var(--clr-accent);
 			}
@@ -154,14 +159,16 @@ export class Button extends LitElement {
 			${this.leading &&
 			html`<lit-icon
 				name=${this.leading}
-				?fill=${this.leading_fill}></lit-icon>`}
+				?fill=${this.leading_fill}
+				part="lead"></lit-icon>`}
 
 			<slot></slot>
 
 			${this.trailing &&
 			html`<lit-icon
 				name=${this.trailing}
-				?fill=${this.trailing_fill}></lit-icon>`}
+				?fill=${this.trailing_fill}
+				part="trail"></lit-icon>`}
 		`;
 	}
 }
--- src/components/organisms/expandable.ts
+++ src/components/organisms/expandable.ts
@@ -14,49 +14,145 @@ export class Expandable extends LitElement {
 	@property({ type: Boolean, reflect: true }) open = false;
 	@property({ type: String }) title = '';
 
-	static styles = css`
-		:host {
-			--idle-bg: transparent;
-			--idle-clr: hsl(0, 0%, 6%);
-			--idle-bord: transparent;
+	static styles = [
+		css`
+			:host {
+				--idle-bg: transparent;
+				--idle-clr: hsl(0, 0%, 6%);
+				--idle-bord: transparent;
 
-			--hover-bg: transparent;
-			--hover-clr: hsl(0, 0%, 6%);
-			--hover-bord: hsl(0, 0%, 96%);
+				--hover-bg: transparent;
+				--hover-clr: hsl(0, 0%, 6%);
+				--hover-bord: hsl(0, 0%, 96%);
 
-			--active-bg: transparent;
-			--active-clr: hsl(0, 0%, 6%);
-			--active-bord: hsl(0, 0%, 90%);
+				--active-bg: transparent;
+				--active-clr: hsl(0, 0%, 6%);
+				--active-bord: hsl(0, 0%, 90%);
 
-			--focus-bg: transparent;
-			--focus-clr: hsl(0, 0%, 6%);
-			--focus-bord: var(--clr-accent);
+				--focus-bg: transparent;
+				--focus-clr: hsl(0, 0%, 6%);
+				--focus-bord: var(--clr-accent);
 
-			box-sizing: border-box;
-			margin: 0;
-			max-width: 100%;
+				box-sizing: border-box;
+				margin: 0;
+				max-width: 100%;
 
-			display: flex;
-			flex-direction: column;
-			overflow: hidden;
-		}
+				display: flex;
+				flex-direction: column;
+				overflow: hidden;
+			}
 
-		:host([rounded]) {
-			border-radius: var(--bord-radius);
-		}
+			:host([rounded]) {
+				border-radius: var(--bord-radius);
+			}
 
-		lit-button {
-			flex-grow: 1;
-			width: 100%;
-		}
+			lit-button {
+				flex-grow: 1;
+				width: 100%;
+			}
 
-		:host > content {
-			display: flex;
-			flex-direction: column;
-			gap: var(--scale-5xs);
-			padding: var(--scale-xs) var(--scale-sm);
-		}
-	`;
+			:host([open]) > lit-button::part(trail) {
+				transform: rotate(90deg);
+			}
+
+			:host > content {
+				display: flex;
+				flex-direction: column;
+				gap: var(--scale-5xs);
+
+				height: 0;
+				min-height: 0;
+				opacity: 0;
+				padding: 0;
+
+				pointer-events: none;
+				user-select: none;
+
+				overflow: hidden;
+
+				transition: opacity var(--transition-settings),
+					padding var(--transition-settings);
+			}
+
+			:host([open]) > content {
+				height: max-content;
+				opacity: 1;
+				padding: var(--scale-xs) var(--scale-sm);
+
+				pointer-events: revert;
+				user-select: revert;
+			}
+		`,
+		css`
+			:host,
+			:host([emphasis='low']) > content > p{
+				background-color: var(--idle-bg);
+				color: var(--idle-clr);
+				outline-color: var(--idle-bord);
+			}
+
+			:host(:focus),
+			:host([emphasis='low']:focus) > content > p {
+				background-color: var(--focus-bg);
+				color: var(--focus-clr);
+				outline-color: var(--focus-bord);
+			}
+
+			@media (hover: hover) {
+				:host(:hover),
+				:host([emphasis='low']:hover) > content > p {
+					background-color: var(--hover-bg);
+					color: var(--hover-clr);
+					outline-color: var(--hover-bord);
+				}
+			}
+
+			:host(:active),
+			:host([emphasis='low']:active) > content > p {
+				background-color: var(--active-bg);
+				color: var(--active-clr);
+				outline-color: var(--active-bord);
+			}
+		`,
+		css`
+			:host([emphasis='medium']) {
+				--idle-bg: hsl(0, 0%, 93%);
+				--idle-clr: hsl(0, 0%, 6%);
+				--idle-bord: transparent;
+
+				--hover-bg: hsl(0, 0%, 96%);
+				--hover-clr: hsl(0, 0%, 6%);
+				--hover-bord: transparent;
+
+				--active-bg: hsl(0, 0%, 87%);
+				--active-clr: hsl(0, 0%, 6%);
+				--active-bord: transparent;
+
+				--focus-bg: hsl(0, 0%, 90%);
+				--focus-clr: hsl(0, 0%, 6%);
+				--focus-bord: var(--clr-accent);
+			}
+		`,
+		css`
+			:host([emphasis='high']) {
+				--idle-bg: hsl(0, 0%, 15%);
+				--idle-clr: hsl(0, 0%, 96%);
+				--idle-bord: transparent;
+
+				--hover-bg: hsl(0, 0%, 21%);
+				--hover-clr: hsl(0, 0%, 96%);
+				--hover-bord: transparent;
+
+				--active-bg: hsl(0, 0%, 9%);
+				--active-clr: hsl(0, 0%, 96%);
+				--active-bord: transparent;
+
+				--focus-bg: hsl(0, 0%, 12%);
+				--focus-clr: hsl(0, 0%, 96%);
+				--focus-bord: var(--clr-accent);
+			}
+		`,
+	];
 
 	protected render() {
 		return html`
@@ -68,7 +164,7 @@ export class Expandable extends LitElement {
 				${this.title}
 			</lit-button>
 
-			${this.open ? html`<content><slot></slot></content>` : ``}
+			<content><slot></slot></content>
 		`;
 	}
 
