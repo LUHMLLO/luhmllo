3b7fbf458698616e7801ed72150814ce90c21b01
Luis Melo <luis.melo@hytechgizmo.com>
2023-11-01 16:01:48 -0400

    small improvements to radiobox, added tabs level-3 component, still empty tho

--- src/common/styles/lit/modules/3/radiobox.ts
+++ src/common/styles/lit/modules/3/radiobox.ts
@@ -2,23 +2,34 @@ import { css } from 'lit';
 
 export default css`
 	:host {
+		border-radius: var(--bordRadius);
 		cursor: pointer;
+
 		display: inline-block;
 		height: max-content;
 
 		isolation: isolate;
+
 		overflow: hidden;
+
 		position: relative;
+
 		user-select: none;
 	}
 
 	:host([checked]):after {
+		border-radius: var(--bordRadius);
 		content: '';
-		inset: 0;
-		position: absolute;
-		z-index: 1;
 
-		outline: red 1px solid;
+		inset: 0;
+
+		outline-color: var(--focus-bord);
 		outline-offset: -1px;
+		outline-style: solid;
+		outline-width: 1px;
+
+		position: absolute;
+
+		z-index: 1;
 	}
 `;
