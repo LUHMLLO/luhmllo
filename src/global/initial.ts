9d586c2e0d6a06b4a7796ac917d2e0a0e1545655
Luis Melo <luis.melo@hytechgizmo.com>
2023-11-13 17:00:23 -0400

    gotta work in a theming system

--- src/global/initial.ts
+++ src/global/initial.ts
@@ -8,24 +8,33 @@ export default css`
 	}
 
 	:host {
+		//idle
 		--idle-bg: transparent;
 		--idle-clr: hsl(0, 0%, 6%);
 		--idle-bord: transparent;
 
+		//hovered
 		--hover-bg: transparent;
 		--hover-clr: hsl(0, 0%, 6%);
 		--hover-bord: hsl(0, 0%, 96%);
 
+		//active / pressed
 		--active-bg: transparent;
 		--active-clr: hsl(0, 0%, 6%);
 		--active-bord: hsl(0, 0%, 90%);
 
+		//focused
 		--focus-bg: transparent;
 		--focus-clr: hsl(0, 0%, 6%);
 		--focus-bord: var(--clr-accent);
+
+		//focused
+		--disabled-bg: transparent;
+		--disabled-clr: hsl(0, 0%, 6%);
+		--disabled-bord: var(--clr-accent);
 	}
 
-	:root {
+	:host {
 		/* scaling */
 		--scale-5xs: 0.3996373225rem;
 		--scale-4xs: 0.4555865477rem;
