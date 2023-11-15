db412d5a1229a3360f344327d62689816dbebe1e
Luis Melo <luis.melo@hytechgizmo.com>
2023-11-15 14:46:30 -0400

    added terser to the build

--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,23 @@ export default defineConfig({
 		},
 	},
 	build: {
+		minify: 'terser',
+		terserOptions: {
+			safari10: true,
+			ie8: true,
+			compress: true,
+			mangle: true,
+		},
+		target: ['es2022'],
+		outDir: 'dist',
+		emptyOutDir: true,
+		cssCodeSplit: true,
+		sourcemap: false,
 		lib: {
 			entry: './src/main.ts',
 			formats: ['es', 'cjs', 'umd', 'iife'],
 			name: 'brrrComponents',
 			fileName: 'index',
-		}
+		},
 	},
 });
