4d3fa1c6ea123941f089db6bc7f6da81f626cf63
LUHMLLO <luhmllo06@protonmail.com>
2024-03-25 13:56:09 -0400

    before replace

--- src/export.css.ts
+++ src/export.css.ts
@@ -1,9 +1,9 @@
-import './lib/css/props.animations.css';
-import './lib/css/props.breakpoints.css';
-import './lib/css/props.settings.css';
-import './lib/css/props.theming.scss';
-import './lib/css/props.typescaling.css';
+import './lib/styles/css/props.animations.css';
+import './lib/styles/css/props.breakpoints.css';
+import './lib/styles/css/props.settings.css';
+import './lib/styles/css/props.theming.css';
+import './lib/styles/css/props.typescaling.css';
 
-import './lib/css/reset.all.css';
-import './lib/css/reset.html.css';
-import './lib/css/reset.scrollbars.css';
+import './lib/styles/css/reset.all.css';
+import './lib/styles/css/reset.html.css';
+import './lib/styles/css/reset.scrollbars.css';
