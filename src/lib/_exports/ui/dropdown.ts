0de0354c2f76c7f4c20ea70d6e2b6e014bc2922e
LUHMLLO <luhmllo06@protonmail.com>
2024-05-23 13:17:46 -0400

    dropdown component it's closer to be finished

--- src/lib/_exports/ui/dropdown.ts
+++ src/lib/_exports/ui/dropdown.ts
@@ -1,27 +1,34 @@
-import { LitElement, css, html, nothing } from 'lit'
-import { customElement, property, query } from 'lit/decorators.js'
+import { LitElement, css, html, nothing } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
 
 import {
-    computePosition,
-    autoUpdate,
-    offset,
-    shift,
-    flip,
-} from '@floating-ui/dom'
+	computePosition,
+	autoUpdate,
+	offset,
+	shift,
+	flip,
+	detectOverflow,
+} from '@floating-ui/dom';
 
-@customElement( 'ly-dropdown' )
+@customElement('ly-dropdown')
 export class Dropdown extends LitElement {
-    @property( { type: Boolean, reflect: true } ) open = false;
+	@property({ type: Boolean, reflect: true }) open = false;
 
-    static override readonly styles = css`
-		:host(:is(ly-dropdown))::part(dropmenu) {
+	static override readonly styles = css`
+		:host(:is(ly-dropdown[open]))::part(dropmenu) {
 			--bg: var(--clr-background);
-            --gap: 0;
+			--gap: 0;
 			--outln-clr: var(--bg);
-            --radius: var(--scale-sm);
+			--radius: var(--scale-sm);
+			--spacing: var(--scale-5xs);
 
-            border-radius: var(--radius);
-            /*clip-path: inset(0% round var(--radius));*/
+			background-color: var(--bg);
+			border-radius: var(--radius);
+			display: grid;
+			grid-template-columns: 1fr;
+			grid-template-rows: 1fr;
+			height: max-content;
+			isolation: isolate;
 			max-height: calc(clamp(16dvh, 25dvh, 32dvh) + var(--scale-5xl));
 			max-width: calc(100dvw - var(--scale-sm));
 			min-height: max-content;
@@ -32,80 +39,145 @@ export class Dropdown extends LitElement {
 					var(--outln-clr),
 					white 3%
 				);
-			/*outline-offset: -0.125rem;*/
+			outline-offset: -0.125rem;
+			overflow: clip;
+			padding: var(--spacing);
+			place-content: center;
 			position: absolute;
 			width: 100%;
 			z-index: 100000;
 		}
-        
-		:host(:is(ly-dropdown)) > ly-slider::-webkit-scrollbar {
-            display: none;
-        }
+
+		:host(:is(ly-dropdown[open]))::part(dropmenu__inner) {
+			--percent: 16%;
+
+			background-color: var(--bg);
+			border-radius: calc(var(--radius) / 2);
+			/*clip-path: inset(0% round var(--radius));*/
+			display: grid;
+			grid-auto-flow: row;
+			grid-auto-rows: max-content;
+			/*mask: linear-gradient(
+				90deg,
+				transparent,
+				black var(--percent),
+				black calc(100% - var(--percent)),
+				transparent
+			);*/
+			/*mask: radial-gradient(circle at center, transparent, var(--bg) var(--percent));*/
+			overflow-x: clip;
+			overflow-y: auto;
+		}
+
+		div::-webkit-scrollbar {
+			display: none;
+		}
+
+		div ::slotted(button) {
+			--radius: calc(var(--scale-sm) / 2) !important;
+			width: 100% !important;
+		}
 	`;
 
-    @query( 'summary' ) private _dropsummary!: HTMLElement
-    @query( 'ly-slider' ) private _dropmenu!: HTMLElement
-    private _cleanup?: any
+	@query('summary') private _dropsummary!: HTMLElement;
+	@query('div[part="dropmenu"]') private _dropmenu!: HTMLElement;
+	private _cleanup?: any;
 
-    override async updated(): Promise<void> {
-        if ( this._dropsummary && this._dropmenu && this.open ) {
-            this._cleanup = autoUpdate(
-                this._dropsummary,
-                this._dropmenu,
-                async () => {
-                    await computePosition( this._dropsummary, this._dropmenu, {
-                        middleware: [ offset( 3 ), flip(), shift() ],
-                        placement: 'bottom',
-                        strategy: 'fixed',
-                    } ).then( ( { x, y } ) => {
-                        Object.assign( this._dropmenu.style, {
-                            left: `${ x }px`,
-                            top: `${ y }px`,
-                        } )
-                    } )
-                },
-                {
-                    ancestorResize: true,
-                    ancestorScroll: true,
-                    animationFrame: true,
-                    elementResize: true,
-                    layoutShift: true,
-                }
-            )
-        } else if ( this._cleanup ) {
-            this._cleanup()
-            this._cleanup = undefined
-        }
-    }
+	override firstUpdated() {
+		document.addEventListener('click', this.clickOutsideHandler);
+	}
 
-    override async disconnectedCallback(): Promise<void> {
-        super.disconnectedCallback()
+	override async updated(): Promise<void> {
+		if (this._dropsummary && this._dropmenu && this.open) {
+			this._cleanup = autoUpdate(
+				this._dropsummary,
+				this._dropmenu,
+				async () => {
+					const { x, y } = await computePosition(
+						this._dropsummary,
+						this._dropmenu,
+						{
+							middleware: [
+								offset(3),
+								flip(),
+								shift(),
+								{
+									name: 'detectOverflowMiddleware',
+									async fn(state) {
+										await detectOverflow(state, {
+											altBoundary: true,
+											padding: 5,
+											boundary: document.documentElement,
+                                            rootBoundary: {
+												x: 0,
+												y: 0,
+												width: document.documentElement.clientWidth,
+												height: document.documentElement.clientHeight,
+											},
+										});
+										return {};
+									},
+								},
+							],
+							placement: 'bottom',
+							strategy: 'fixed',
+						}
+					);
+					Object.assign(this._dropmenu.style, {
+						left: `${x}px`,
+						top: `${y}px`,
+					});
+				}
+			);
+		} else if (this._cleanup) {
+			this._cleanup();
+			this._cleanup = undefined;
+		}
+	}
 
-        if ( this._cleanup ) {
-            this._cleanup()
-        }
-    }
+	override async disconnectedCallback(): Promise<void> {
+		super.disconnectedCallback();
+		document.removeEventListener('click', this.clickOutsideHandler.bind(this));
 
-    protected override render() {
-        return html`
-			<summary>
-				<slot name="summary" @click=${ this._toggleOpen }></slot>
+		if (this._cleanup) {
+			this._cleanup();
+		}
+	}
+
+	protected override render() {
+		return html`
+			<summary @click=${this._toggleOpen}>
+				<slot name="summary"></slot>
 			</summary>
-			${ this.open
-                ? html`
-						<ly-slider axis="col" part="dropmenu">
-							<slot></slot>
-						</ly-slider>
+			${this.open
+				? html`
+						<div part="dropmenu">
+							<div part="dropmenu__inner">
+								<slot></slot>
+							</div>
+						</div>
 				  `
-                : nothing }
-		`
-    }
+				: nothing}
+		`;
+	}
 
-    private _toggleOpen() {
-        if ( this._cleanup ) {
-            this._cleanup()
-        }
+	private _toggleOpen() {
+		if (this._cleanup) {
+			this._cleanup();
+		}
 
-        this.open = !this.open
-    }
+		this.open = !this.open;
+	}
+
+	// Added clickOutside handler
+	clickOutsideHandler(event: MouseEvent) {
+		const target = event.target as Node;
+		if (
+			this.open &&
+			target &&
+			(!this._dropsummary?.contains(target) || !this._dropmenu.contains(target))
+		) {
+			this.open = false;
+		}
+	}
 }
