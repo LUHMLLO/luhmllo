49a28552bc2eaae980e6b06c83cbc52c2ba51f2f
LUHMLLO <luisrmelo@proton.me>
2024-11-21 23:36:17 -0400

    anoda one, local works, jsr not

--- v/jsr/@lilycat/mod.ts
+++ v/jsr/@lilycat/mod.ts
@@ -1,7 +1,4 @@
-// mod.ts
-import * as path from "jsr:@std/path@1.0.8";
-
-// Use relative path instead of URL for file reading
-const cssPath = path.join(path.dirname(path.fromFileUrl(import.meta.url)), "dist", "all.css");
-export const styles: string = await Deno.readTextFile(cssPath);
+// mod.ts - simplest approach
+const __dirname = new URL(".", import.meta.url).pathname;
+export const styles: string = await Deno.readTextFile(`${__dirname}/dist/all.css`);
 export default styles;
