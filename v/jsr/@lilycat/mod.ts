038dcb852f41773b0d7edb3098fa96cdf2b65ae6
LUHMLLO <luisrmelo@proton.me>
2024-11-21 23:23:39 -0400

    coniazo

--- v/jsr/@lilycat/mod.ts
+++ v/jsr/@lilycat/mod.ts
@@ -1,5 +1,5 @@
 // mod.ts
-import * as path from "jsr:@std/path";
+import * as path from "jsr:@std/path@1.0.8";
 
 const cssPath = path.fromFileUrl(new URL("./dist/all.css", import.meta.url));
 export const styles: string = await Deno.readTextFile(cssPath);
