7b48cd14adb6fda912e6013f13ebe72f4548a471
LUHMLLO <luisrmelo@proton.me>
2025-02-22 21:35:50 -0400

    fixed color tokens using wrong hsl format, updated tailwind custom vars names, still working on reset.css

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,41 +1,36 @@
 import { type Config } from "tailwindcss";
 
-const Scales = {
-  "xs": "var(--scale-xs)",
-  "sm": "var(--scale-sm)",
-  "md": "var(--scale-md)",
-  "nm": "var(--scale-nm)",
-  "lg": "var(--scale-lg)",
-  "xl": "var(--scale-xl)",
-};
-
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
   theme: {
     colors: {
-      "accent--1": "var(--clr-accent-1)",
-      "accent--2": "var(--clr-accent-2)",
-      "accent--3": "var(--clr-accent-3)",
-      "accent--4": "var(--clr-accent-4)",
+      "accent": "var(--clr-accent)",
+      "context": "var(--clr-context)",
+      "content": "var(--clr-content)",
+      "surface": "var(--clr-surface)",
 
-      "context--1": "var(--clr-context-1)",
-      "context--2": "var(--clr-context-2)",
-      "context--3": "var(--clr-context-3)",
-      "context--4": "var(--clr-context-4)",
-
-      "content--1": "var(--clr-content-1)",
-      "content--2": "var(--clr-content-2)",
-      "content--3": "var(--clr-content-3)",
-      "content--4": "var(--clr-content-4)",
-
-      "surface--1": "var(--clr-surface-1)",
-      "surface--2": "var(--clr-surface-2)",
-      "surface--3": "var(--clr-surface-3)",
-      "surface--4": "var(--clr-surface-4)",
+      "debug": "var(--clr-debug)",
+      "error": "var(--clr-error)",
+      "info": "var(--clr-info)",
+      "success": "var(--clr-success)",
     },
-    fontSize: Scales,
-    spacing: Scales,
+    fontSize: {
+      "xs": "var(--type-xs)",
+      "sm": "var(--type-sm)",
+      "md": "var(--type-md)",
+      "nm": "var(--type-nm)",
+      "lg": "var(--type-lg)",
+      "xl": "var(--type-xl)",
+    },
+    spacing: {
+      "xs": "var(--xs)",
+      "sm": "var(--sm)",
+      "md": "var(--md)",
+      "nm": "var(--nm)",
+      "lg": "var(--lg)",
+      "xl": "var(--xl)",
+    },
   },
 } satisfies Config;
