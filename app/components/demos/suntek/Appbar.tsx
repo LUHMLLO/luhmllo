fb95e836bde7e007726666017f47fd1dcf103eec
LUHMLLO <luhmllo06@protonmail.com>
2025-03-12 15:50:14 -0400

    created --wrapper data prop for anchors <a/> with small workaround for chromium browser delaying nested transitions

--- app/components/demos/suntek/Appbar.tsx
+++ app/components/demos/suntek/Appbar.tsx
@@ -8,39 +8,65 @@ export default function Appbar() {
       <hr />
 
       <nav className="flex">
-        <a href={void 0}>Agenda</a>
+        <a href={void 0}>Calendars</a>
+        <a href={void 0}>Dashboards</a>
+      </nav>
+
+      <hr />
+
+      <nav className="flex">
         <a href={void 0}>Pipelines</a>
-        <a href={void 0}>Sales</a>
         <a href={void 0}>Tasks</a>
         <a href={void 0}>Workflows</a>
       </nav>
 
       <hr />
 
+      <nav className="flex">
+        <a href={void 0}>Campaigns</a>
+        <a href={void 0}>Contacts</a>
+        <a href={void 0}>Projects</a>
+      </nav>
+
+      <hr />
+
+      <nav className="flex">
+        <a href={void 0}>Leads</a>
+        <a href={void 0}>Opportunities</a>
+        <a href={void 0}>Quotes</a>
+        <a href={void 0}>Sales</a>
+      </nav>
+
+      <hr />
+
       <nav className="flex flex-grow flex-shrink">
-        {[
-          "Contacts",
-          "Leads",
-          "Opportunities",
-          "Quotes",
-          "Reports",
-        ].map((link, i) => {
-          return <a href={void 0} key={i + "-" + link}>{link}</a>;
-        })}
+        <a href={void 0}>Inventories</a>
+        <a href={void 0}>Products</a>
+        <a href={void 0}>Services</a>
         <i className="icon">more_horiz</i>
       </nav>
 
       <hr />
 
       <nav className="flex">
-        <i className="icon">search</i>
-        <i className="icon">add_2</i>
-        <i className="icon">notifications</i>
-        <i className="icon">settings</i>
+        <a href={void 0} data-props="--wrapper">
+          <i className="icon">search</i>
+        </a>
+        <a href={void 0} data-props="--wrapper">
+          <i className="icon">add_2</i>
+        </a>
+        <a href={void 0} data-props="--wrapper">
+          <i className="icon">inbox</i>
+        </a>
+        <a href={void 0} data-props="--wrapper">
+          <i className="icon">settings</i>
+        </a>
         <figure className="aspect-[1] h-xl shrink-0 rounded w-xl">
           <img src="/media/suntek/avatar.png" alt="avatar" />
         </figure>
-        <i className="icon">apps</i>
+        <a href={void 0} data-props="--wrapper">
+          <i className="icon">apps</i>
+        </a>
       </nav>
     </nav>
   );
