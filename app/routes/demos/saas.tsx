770869f006e2a400096c88455addcadada8dbf8d
LUHMLLO <luhmllo06@protonmail.com>
2025-03-19 09:07:26 -0400

    renamed all `export default function Route` to `export default function Page`

--- app/routes/demos/saas.tsx
+++ app/routes/demos/saas.tsx
@@ -3,7 +3,7 @@ interface FoundersArticle {
   paragraph: string;
 }
 
-export default function Route() {
+export default function Page() {
   const logos: string[] = [
     "https://cdn.freebiesupply.com/logos/large/2x/apple-logo-png-transparent.png",
     "https://cdn.freebiesupply.com/logos/large/2x/microsoft-logo-png-transparent.png",
--- app/routes/demos/suntek/index.tsx
+++ app/routes/demos/suntek/index.tsx
@@ -1,4 +1,4 @@
-export default function Route() {
+export default function Page() {
   return (
     <div
       id="views"
