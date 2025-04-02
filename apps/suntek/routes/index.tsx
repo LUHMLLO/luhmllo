ecc3805b33856d98328f7a963b6c0d873bdbce7e
LUHMLLO <luhmllo06@protonmail.com>
2025-04-02 09:55:56 -0400

    newer attempt to reduce the amount of apps living inside lilycat repo

--- apps/suntek/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { type PageProps } from "$fresh/server.ts";
-
-export default function Page(_props: PageProps) {
-  return (
-    <div id="views">
-      hello world <code>{_props.url.pathname}</code>
-    </div>
-  );
-}
