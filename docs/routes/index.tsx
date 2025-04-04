714414706a6341da88609708d44cd664323a335f
LUHMLLO <luisrmelo@proton.me>
2025-04-03 22:45:29 -0400

    gotta do a mess before making art

--- docs/routes/index.tsx
+++ docs/routes/index.tsx
@@ -16,12 +16,32 @@ const routes = [
 export default function Page(_props: PageProps) {
   return (
     <>
+      <nav className="items-center gap-md place-between p-md w-full z-10">
+        <a href="/">
+          <figure className="aspect-[1/1] h-5xl">
+            <img src="/media/logo.svg" alt="logo" />
+          </figure>
+        </a>
+
+        <nav className="items-center gap-md place-content-end grow">
+          <a href="/" className="text-inherit">Get Started</a>
+          <a href="/test" className="text-inherit">HTML Tags</a>
+          <button type="button" disabled>
+            Playground (not available)
+          </button>
+        </nav>
+      </nav>
+
       <header>
         <h1>Lilycat</h1>
-        <p>Keeping styling where it belongs</p>
+        <p>
+          For better human interfaces &amp; user experiences while keeping
+          styling where it belongs
+        </p>
+        <p>
+          Powered by dead simple CSS props and a mix of common convention rules.
+        </p>
       </header>
-      <span>Native Elements</span>
-      <h2>Learn their behaviour</h2>
       <x-grid className="gap-md grid-cols-2">
         {routes.map((path) => (
           <a href={"/el/" + path} className="w-full">
