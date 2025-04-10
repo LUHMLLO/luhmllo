1b101632c341147ee35e7812255fc9b7c32fc587
LUHMLLO <luisrmelo@proton.me>
2025-04-09 22:46:41 -0400

    text nodes inside buttons must be wrapped on another element for the .icon first-child and .icon last-child conditions to be met correctly

--- docs/routes/el/button.tsx
+++ docs/routes/el/button.tsx
@@ -4,23 +4,32 @@ export default function Page(_props: PageProps) {
   return (
     <>
       <button type="button">
-        <i className="icon">asterisk</i>lead icon
+        <i className="icon">arrow_left_alt</i>
+        <span>lead icon</span>
       </button>
 
       <button type="button">no icons</button>
 
       <button type="button">
-        <i className="icon">asterisk</i>both icons<i className="icon">
-          asterisk
+        <i className="icon">add_2</i>
+      </button>
+
+      <button type="button">
+        <i className="icon">globe</i>
+        <span>both icons</span>
+        <i className="icon">
+          open_in_new
         </i>
       </button>
 
       <button type="button">
-        trail icon<i className="icon">asterisk</i>
+        <span>trail icon</span>
+        <i className="icon">arrow_right_alt</i>
       </button>
 
       <button type="button" disabled>
-        disabled<i className="icon">block</i>
+        <span>disabled</span>
+        <i className="icon">block</i>
       </button>
     </>
   );
