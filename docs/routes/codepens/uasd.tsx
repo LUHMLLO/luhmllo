eec0fd72a97e727057c6b1ad8e02d047c173cdee
LUHMLLO <luisrmelo@proton.me>
2025-04-12 13:56:04 -0400

    working on uasd

--- docs/routes/codepens/uasd.tsx
+++ docs/routes/codepens/uasd.tsx
@@ -9,8 +9,14 @@ export default function Page(_props: PageProps) {
         href={asset("/styles/codepens/uasd.css")}
       />
 
-      <x-block id="appbar">
-        <x-flex>
+      <x-stack
+        id="appbar"
+        style="background-color: var(--clr-primary); gap: var(--md, 15px); padding: var(--md, 15px) var(--sm, 10px);"
+      >
+        <x-flex style="gap: var(--sm, 10px); place-items: center;">
+          <button type="button" data-props="--primary">
+            <i className="icon">arrow_back</i>
+          </button>
           <figure style="flex-shrink: 1; width: min(100%, 100px);">
             <img
               src="/media/logo_uasd.svg"
@@ -19,15 +25,23 @@ export default function Page(_props: PageProps) {
             />
           </figure>
 
-          <x-group style="flex: 1 1; place-content: center; place-items: center; width: 100%;">
+          <x-row style="flex: 1 1; gap: inherit; place-content: end; place-items: center; width: 100%;">
+            <a href={"#" + crypto.randomUUID()}>Intranet</a>
+            <a href={"#" + crypto.randomUUID()}>Contactos</a>
+            <a href={"#" + crypto.randomUUID()}>Correo</a>
+            <a href={"#" + crypto.randomUUID()}>Egresados</a>
+            <a href={"#" + crypto.randomUUID()}>Revistas Academicas</a>
             <button type="button" data-props="--accent">AUTOSERVICIO</button>
-          </x-group>
+            <button type="button" data-props="--secondary">
+              <i className="icon">menu</i>
+            </button>
+          </x-row>
         </x-flex>
 
         <label
           htmlFor="searchbar"
           data-props="--input"
-          style="border-radius: 1000px;"
+          style="--inputBackground: var(--clr-secondary); border-radius: 1000px;"
         >
           <input
             type="text"
@@ -39,26 +53,30 @@ export default function Page(_props: PageProps) {
             <i className="icon">search</i>
           </button>
         </label>
+      </x-stack>
 
-        <hr />
+      <figure style="height: 512px; width: 100%;">
+        <img src="https://images.unsplash.com/photo-1519452635265-7b1fbfd1e4e0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="banner" />
+      </figure>
 
-        <x-row style="align-items: start;">
-          <i class="icon" style="--fill: 1; --size: var(--xl);">
-            schedule
-          </i>
-          <p>
-            Leave on Time Sensitive notifications from Props? This allows Props
-            to deliver important notifications inmmediately
-          </p>
-        </x-row>
+      <hr />
 
-        <hr />
+      <x-row style="align-items: start;">
+        <i class="icon" style="--fill: 1; --size: var(--xl);">
+          schedule
+        </i>
+        <p>
+          Leave on Time Sensitive notifications from Props? This allows Props to
+          deliver important notifications inmmediately
+        </p>
+      </x-row>
 
-        <x-row style="flex-wrap: wrap;">
-          <button type="button">Turn Off</button>
-          <button type="button">Leave On</button>
-        </x-row>
-      </x-block>
+      <hr />
+
+      <x-row style="flex-wrap: wrap;">
+        <button type="button">Turn Off</button>
+        <button type="button">Leave On</button>
+      </x-row>
     </>
   );
 }
