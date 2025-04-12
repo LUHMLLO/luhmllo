4b7f057cb1d42742393fda2a7a014516726c6a07
LUHMLLO <luisrmelo@proton.me>
2025-04-12 14:49:28 -0400

    more ideas incoming

--- docs/routes/codepens/uasd.tsx
+++ docs/routes/codepens/uasd.tsx
@@ -26,11 +26,21 @@ export default function Page(_props: PageProps) {
           </figure>
 
           <x-row style="flex: 1 1; gap: inherit; place-content: end; place-items: center; width: 100%;">
-            <a href={"#" + crypto.randomUUID()}>Intranet</a>
-            <a href={"#" + crypto.randomUUID()}>Contactos</a>
-            <a href={"#" + crypto.randomUUID()}>Correo</a>
-            <a href={"#" + crypto.randomUUID()}>Egresados</a>
-            <a href={"#" + crypto.randomUUID()}>Revistas Academicas</a>
+            <a href={"#" + crypto.randomUUID()} style="color: inherit;">
+              Intranet
+            </a>
+            <a href={"#" + crypto.randomUUID()} style="color: inherit;">
+              Contactos
+            </a>
+            <a href={"#" + crypto.randomUUID()} style="color: inherit;">
+              Correo
+            </a>
+            <a href={"#" + crypto.randomUUID()} style="color: inherit;">
+              Egresados
+            </a>
+            <a href={"#" + crypto.randomUUID()} style="color: inherit;">
+              Revistas Academicas
+            </a>
             <button type="button" data-props="--accent">AUTOSERVICIO</button>
             <button type="button" data-props="--secondary">
               <i className="icon">menu</i>
@@ -56,7 +66,10 @@ export default function Page(_props: PageProps) {
       </x-stack>
 
       <figure style="height: 512px; width: 100%;">
-        <img src="https://images.unsplash.com/photo-1519452635265-7b1fbfd1e4e0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="banner" />
+        <img
+          src="https://images.unsplash.com/photo-1519452635265-7b1fbfd1e4e0?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="banner"
+        />
       </figure>
 
       <hr />
