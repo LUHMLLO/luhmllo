eac3008f82604617d8837213b64e57c2ec31d040
LUHMLLO <luhmllo06@protonmail.com>
2025-04-14 11:41:21 -0400

    working on lilycat website

--- docs/routes/index.tsx
+++ docs/routes/index.tsx
@@ -15,55 +15,48 @@ const routes = [
 
 export default function Page(_props: PageProps) {
   return (
-    <>
-      <x-stack style="gap: var(--nm); padding: var(--md, 15px);">
-        <nav id="docs-appbar" style="place-items: center; position: sticky; top: 0; z-index: 10;">
-          <a
-            href="/"
-            style=" text-box: none; "
-          >
-            <figure style="aspect-ratio: 1/1; width: 50px;">
-              <img src="/media/lilycat.png" alt="logo" />
-            </figure>
+    <div id="docs-app">
+      <nav id="docs-appbar">
+        <a href="/">
+          <figure>
+            <img src="/media/lilycat.png" alt="logo" />
+          </figure>
+        </a>
+
+        <x-row style="gap: var(--md, 15px); place-content: end; place-items: center; width: 100%;">
+          <button type="button" style="display: contents;">
+            menu
+          </button>
+        </x-row>
+      </nav>
+
+      <header>
+        <h1>Lilycat</h1>
+        <p>
+          For better human interfaces &amp; user experiences while keeping
+          styling where it belongs. Powered by dead simple CSS props and a mix
+          of common convention rules.
+        </p>
+      </header>
+
+      <x-grid style="gap: var(--md); grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));">
+        {routes.map((path) => (
+          <a href={"/el/" + path}>
+            <x-card>
+              <x-flex>
+                <span>
+                  {path}
+                </span>
+                <span>
+                  <i className="icon">
+                    north_east
+                  </i>
+                </span>
+              </x-flex>
+            </x-card>
           </a>
-
-          <x-row style="gap: var(--md, 15px); place-content: end; place-items: center; width: 100%;">
-            <button type="button">
-              <i className="icon">
-                menu
-              </i>
-            </button>
-          </x-row>
-        </nav>
-
-        <header>
-          <h1>Lilycat</h1>
-          <p>
-            For better human interfaces &amp; user experiences while keeping
-            styling where it belongs. Powered by dead simple CSS props and a mix
-            of common convention rules.
-          </p>
-        </header>
-
-        <x-grid style="gap: var(--md); grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));">
-          {routes.map((path) => (
-            <a href={"/el/" + path}>
-              <x-card>
-                <x-flex>
-                  <span>
-                    {path}
-                  </span>
-                  <span>
-                    <i className="icon">
-                      north_east
-                    </i>
-                  </span>
-                </x-flex>
-              </x-card>
-            </a>
-          ))}
-        </x-grid>
-      </x-stack>
-    </>
+        ))}
+      </x-grid>
+    </div>
   );
 }
