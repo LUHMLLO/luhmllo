3700efed57b5632bd6d5f4b97680bb5126e2467a
LUHMLLO <luisrmelo@proton.me>
2025-05-02 20:31:37 -0400

    organized assets inside the public folder

--- docs/v2/astro.config.mjs
+++ docs/v2/astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import svelte from '@astrojs/svelte'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   scopedStyleStrategy: "where",
-  integrations: [ mdx({
-    shikiConfig: { theme: "vitesse-dark" },
-  }), svelte() ],
-})
+  integrations: [
+    mdx({
+      shikiConfig: { theme: "vitesse-dark" },
+    }),
+    svelte(),
+  ],
+});
