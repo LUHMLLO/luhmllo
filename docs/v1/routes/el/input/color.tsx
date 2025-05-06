10699c26dac91eb33bf06c1e02e856d1af38f215
LUHMLLO <luhmllo06@protonmail.com>
2025-05-06 12:05:44 -0400

    cleaning thins up

--- docs/v1/routes/el/input/color.tsx
+++
@@ -1,9 +0,0 @@
-import { type PageProps } from "$fresh/server.ts";
-
-export default function Page(_props: PageProps) {
-  return (
-    <x-grid style="gap: var(--nm); grid-auto-rows: max-content; min-height: 100dvh; place-content: safe center;">
-      <input type="color" id="ic" value="#c22424" />
-    </x-grid>
-  );
-}
