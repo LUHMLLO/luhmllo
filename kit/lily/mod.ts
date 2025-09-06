0af8cb5c5f864df393664d9038a35e809853b7c4
LUHMLLO <luisrmelo@proton.me>
2025-09-06 15:58:34 -0400

    updated many data-attributes

--- kit/lily/mod.ts
+++ kit/lily/mod.ts
@@ -1,10 +1,19 @@
 // @ts-nocheck - Bundled outputs
-export const all = "./dist/all.css";
-export const layout = "./dist/layout.css";
-export const normalize = "./dist/normalize.css";
-export const reset = "./dist/reset.css";
 
 // export { default as all } from "./dist/all.css" with { type: "css" };
 // export { default as layout } from "./dist/layout.css" with { type: "css" };
 // export { default as normalize } from "./dist/normalize.css" with { type: "css" };
 // export { default as reset } from "./dist/reset.css" with { type: "css" };
+
+export const all = Deno.readTextFileSync(
+  new URL("./dist/all.css", import.meta.url),
+) as string;
+export const layout = Deno.readTextFileSync(
+  new URL("./dist/layout.css", import.meta.url),
+) as string;
+export const normalize = Deno.readTextFileSync(
+  new URL("./dist/normalize.css", import.meta.url),
+) as string;
+export const reset = Deno.readTextFileSync(
+  new URL("./dist/reset.css", import.meta.url),
+) as string;
