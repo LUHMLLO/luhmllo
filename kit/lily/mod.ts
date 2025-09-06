292d8502e1935f3857c50ccd546e8b34a27199d7
LUHMLLO <luisrmelo@proton.me>
2025-09-06 13:06:52 -0400

    allegedly this should work

--- kit/lily/mod.ts
+++ kit/lily/mod.ts
@@ -1,5 +1,12 @@
+// mod.ts - Main entry point for @luhmllo/lily
 // @ts-nocheck - Bundled outputs
-export * from "./dist/all.css";
-export * from "./dist/layout.css";
-export * from "./dist/normalize.css";
-export * from "./dist/reset.css";
+
+// Named exports for destructuring imports
+export { default as all } from "./dist/all.css" with { type: "css" };
+export { default as layout } from "./dist/layout.css" with { type: "css" };
+export { default as normalize } from "./dist/normalize.css" with { type: "css" };
+export { default as reset } from "./dist/reset.css" with { type: "css" };
+
+// Package metadata
+export const version = "0.0.1";
+export const name = "@luhmllo/lily";
